e8393c9ef1dbea9ac88d17a290f28920c582ea92
tymoteuszratajczak <tymoteusz.ratajczak@outlook.com>
2025-10-30 20:47:55 +0100

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -7,15 +7,15 @@ export const CREATE_SESSION_ENDPOINT = "/api/create-session";
 
 export const STARTER_PROMPTS: StartScreenPrompt[] = [
   {
-    label: "What can you do?",
-    prompt: "What can you do?",
+    label: "Czego chcesz się nauczyć?",
+    prompt: "Czego możesz mnie nauczyć?",
     icon: "circle-question",
   },
 ];
 
-export const PLACEHOLDER_INPUT = "Ask anything...";
+export const PLACEHOLDER_INPUT = "Zadaj mi pytanie...";
 
-export const GREETING = "How can I help you today?";
+export const GREETING = "Cześć! Rozpoczynamy naukę?";
 
 export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
   color: {
